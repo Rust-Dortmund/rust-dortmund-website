6025db433174eaacc187238270b61625e1b63724
Tim Janus <tim@janus.rs>
2025-06-27 12:19:13 +0200

    Restructure into workspace (#1)
    * restructure in workspace structure
    
    * configure trunk depending on folder
    
    * add an example backend
    
    * use workspace.dependencies for frontend-yew

---
+++ backend/example/src/main.rs
@@ -0,0 +1,17 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, I'm an example backend!" }));
+
+    let ip = "127.0.0.1"; // localhost only;
+    //let ip = "0.0.0.0"; // listens on every network interface
+
+    let port = 3000; // default port for the example backend
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind(format!("{ip}:{port}")).await.unwrap();
+    println!("Listening on http://{}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
