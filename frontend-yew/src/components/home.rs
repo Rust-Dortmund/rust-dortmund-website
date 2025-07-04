6ba03b4e2ba6626b6fbc955d6690c4d16b607841
Tim Janus <tim@janus.rs>
2025-07-04 16:59:05 +0200

    overwork syle and fix text on Home

--- frontend-yew/src/components/home.rs
+++ frontend-yew/src/components/home.rs
@@ -1,4 +1,4 @@
-use crate::app::Route;
+use crate::nav::Route;
 use crate::models::State;
 use yew::{classes, function_component, html, Html};
 use yew_router::prelude::*;
@@ -18,9 +18,9 @@ pub fn home() -> Html {
             <Link<Route> classes={classes!("btn")} to={Route::UpcomingEventListRequest }>
                     { "View Events" }</Link<Route>>
         </div>
-            <img class="logo" src="./assets/rust.webp" alt="rust do logo"  style="max-width:60vw;height:auto;border-radius:30px" />
+            <img class="logo" src="./assets/rust.webp" alt="Rust Dortmund Meetup logo"  style="max-width:60vw;height:auto;border-radius:30px" />
             <br />
-            <span class="subtitle">{ "this website is made in Dortmund with " }<span class="heart" />{" and "}<span class="crab" /></span>
+            <span class="subtitle">{ "This website is made in Dortmund with " }<span class="heart" />{" and "}<span class="crab" /></span>
         </>
     }
 }
